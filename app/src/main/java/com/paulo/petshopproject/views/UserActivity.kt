fc9127e9c242b7e5dd58d49aef2a48d706aaf651
caiosbras <caiosbras@hotmail.com>
2020-06-04 14:12:17 -0300

    Exibição de itens atualizada

---
+++ app/src/main/java/com/paulo/petshopproject/views/UserActivity.kt
@@ -0,0 +1,12 @@
+package com.paulo.petshopproject.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.paulo.petshopproject.R
+
+class UserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user)
+    }
+}
